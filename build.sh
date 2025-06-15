d97b3552e89e388bc05b4bac643bb28d18c70ac5
Manish kumar yadav <120595460+manish6299@users.noreply.github.com>
2025-06-15 13:17:10 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+
+# Step 1: Update apt and install portaudio
 apt-get update
 apt-get install -y portaudio19-dev
+
+# Step 2: Install Python dependencies
 pip install -r requirements.txt
